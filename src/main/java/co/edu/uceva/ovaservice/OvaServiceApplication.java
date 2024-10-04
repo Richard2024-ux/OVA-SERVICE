a64512d503ce4a1138468feeb54ce57095f4c4c9
Richard Rivera <richard.rivera01@uceva.edu.co>
2024-10-03 23:17:20 -0500

    codigo base

---
+++ src/main/java/co/edu/uceva/ovaservice/OvaServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.ovaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvaServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uceva/ovaservice/OvaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.ovaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
