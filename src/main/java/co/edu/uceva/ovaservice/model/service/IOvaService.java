c0881094dba8068975ca7d590c350a9b190b306a
Juan Esteban Tobon <juan.tobon01@uceva.edu.co>
2024-10-11 11:49:01 -0500

    Se implemento el metodo Actualizar

--- src/main/java/co/edu/uceva/ovaservice/model/service/IOvaService.java
+++ src/main/java/co/edu/uceva/ovaservice/model/service/IOvaService.java
@@ -8,6 +8,6 @@ public interface IOvaService {
     void delete(Ova ova); // Elimina un OVA de la base de datos
     Ova save(Ova ova); // Guarda un OVA y me retorna un objeto de tipo Ova
     Ova findById(Long id); // Busca un OVA por su id y me retorna un objeto de tipo Ova
-    Ova update(Ova ova); // Actualiza un OVA y me retorna un objeto de tipo Ova
+    Ova Update(Ova ova); // Actualiza un OVA y me retorna un objeto de tipo Ova
 }
 
